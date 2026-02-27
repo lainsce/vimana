ebb944c47225d29dfc90743316b8eaca7fb77261
lainsce <paulogalardi1@gmail.com>
2026-02-27 08:49:27 -0300

    Add dictionary support and refactor YisVal

--- src/src/yis_compat.h
+++ src/src/yis_compat.h
@@ -12,6 +12,7 @@ typedef enum {
   EVT_BOOL,
   EVT_STR,
   EVT_ARR,
+  EVT_DICT,
   EVT_OBJ,
   EVT_FN
 } YisTag;
@@ -36,15 +37,8 @@ typedef struct YisObj {
   void (*drop)(struct YisObj* o);
 } YisObj;
 
-typedef struct YisFn {
-  int ref;
-  int arity;
-  YisVal (*fn)(void* env, int argc, YisVal* argv);
-  void* env;
-} YisFn;
-
-typedef YisVal (*YisFnImpl)(void* env, int argc, YisVal* argv);
-
+/* YisVal must be fully defined before YisDictEnt (which embeds it by value)
+   and before YisFn (which uses it in a function-pointer signature). */
 struct YisVal {
   YisTag tag;
   union {
@@ -55,14 +49,36 @@ struct YisVal {
   } as;
 };
 
-#define YV_NULLV ((YisVal){ .tag = EVT_NULL })
-#define YV_INT(v) ((YisVal){ .tag = EVT_INT, .as.i = (int64_t)(v) })
-#define YV_FLOAT(v) ((YisVal){ .tag = EVT_FLOAT, .as.f = (double)(v) })
-#define YV_BOOL(v) ((YisVal){ .tag = EVT_BOOL, .as.b = (bool)(v) })
-#define YV_STR(v) ((YisVal){ .tag = EVT_STR, .as.p = (void*)(v) })
-#define YV_ARR(v) ((YisVal){ .tag = EVT_ARR, .as.p = (void*)(v) })
-#define YV_OBJ(v) ((YisVal){ .tag = EVT_OBJ, .as.p = (void*)(v) })
-#define YV_FN(v) ((YisVal){ .tag = EVT_FN, .as.p = (void*)(v) })
+typedef struct YisDictEnt {
+  YisStr* key;
+  YisVal val;
+} YisDictEnt;
+
+typedef struct YisDict {
+  int ref;
+  size_t len;
+  size_t cap;
+  YisDictEnt* entries;
+} YisDict;
+
+typedef struct YisFn {
+  int ref;
+  int arity;
+  YisVal (*fn)(void* env, int argc, YisVal* argv);
+  void* env;
+} YisFn;
+
+typedef YisVal (*YisFnImpl)(void* env, int argc, YisVal* argv);
+
+#define YV_NULLV      ((YisVal){ .tag = EVT_NULL })
+#define YV_INT(v)     ((YisVal){ .tag = EVT_INT,   .as.i = (int64_t)(v) })
+#define YV_FLOAT(v)   ((YisVal){ .tag = EVT_FLOAT, .as.f = (double)(v)  })
+#define YV_BOOL(v)    ((YisVal){ .tag = EVT_BOOL,  .as.b = (bool)(v)    })
+#define YV_STR(v)     ((YisVal){ .tag = EVT_STR,   .as.p = (void*)(v)   })
+#define YV_ARR(v)     ((YisVal){ .tag = EVT_ARR,   .as.p = (void*)(v)   })
+#define YV_DICT(v)    ((YisVal){ .tag = EVT_DICT,  .as.p = (void*)(v)   })
+#define YV_OBJ(v)     ((YisVal){ .tag = EVT_OBJ,   .as.p = (void*)(v)   })
+#define YV_FN(v)      ((YisVal){ .tag = EVT_FN,    .as.p = (void*)(v)   })
 
 void* cogito_compat_obj_new(size_t size, void (*drop)(YisObj* o));
 void cogito_compat_retain_val(YisVal v);
@@ -70,8 +86,8 @@ void cogito_compat_release_val(YisVal v);
 void cogito_compat_trap(const char* msg);
 
 int64_t cogito_compat_as_int(YisVal v);
-double cogito_compat_as_float(YisVal v);
-bool cogito_compat_as_bool(YisVal v);
+double  cogito_compat_as_float(YisVal v);
+bool    cogito_compat_as_bool(YisVal v);
 
 YisVal cogito_compat_call(YisVal fnv, int argc, YisVal* argv);
 
@@ -80,5 +96,5 @@ YisStr* cogito_compat_str_lit(const char* s);
 YisStr* cogito_compat_to_string(YisVal v);
 
 YisArr* cogito_compat_arr_new(size_t len);
-void cogito_compat_arr_set(YisArr* a, size_t idx, YisVal v);
-YisVal cogito_compat_arr_get(YisArr* a, int64_t idx);
+void    cogito_compat_arr_set(YisArr* a, size_t idx, YisVal v);
+YisVal  cogito_compat_arr_get(YisArr* a, int64_t idx);
